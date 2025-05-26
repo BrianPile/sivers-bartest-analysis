31802b9b6c13e7fca3800db1b02972f3a111e5d3
Brian Pile <brian.pile@gmail.com>
2025-05-27 07:44:41 +0800

    modify dev_summarization_chenker script to use here() for file paths

--- src/dev summarization - chunker.R
+++ src/dev summarization - chunker.R
@@ -1,15 +1,16 @@
 # Sivers LOT Data Summarization
 library(tidyverse)
+library(here)
 library(devparext)
 library(readxl)
 library(writexl)
 rm(list = ls())
 
 # source the config file
-source("./config_info.R")
+source(here("config_info.R"))
 
 #### LIV DATA SUMMARIZATION ####
-df_liv = data.table::fread(file = paste0("./data/", config_info$lotID, "_combined_LIV.csv"))
+df_liv = data.table::fread(here("data/processed", paste0(config_info$lotID, "_combined_LIV.csv")))
 
 df_liv = df_liv |> 
   group_by(filename, lotID, SN, tempC) |> 
@@ -26,14 +27,14 @@ idx2 = idx1 + chunk_len - 1
 max_group_id_liv = max(unique(df_liv$group_id))
 append_mode = FALSE
 while (TRUE) {
-
+  
   if (idx2 > max_group_id_liv) {
     idx2 = max_group_id_liv
   }
   
   # write message updates to console
-  print(paste0("idx1 = ", idx1))
-  print(paste0("idx2 = ", idx2))
+  message(paste0("idx1 = ", idx1))
+  message(paste0("idx2 = ", idx2))
   
   this_df_liv = df_liv |> 
     filter(group_id %in% idx1:idx2)
@@ -47,10 +48,10 @@ while (TRUE) {
     tidyfast::dt_separate(SN, into = c("waferID", "cellID", "barID", "dieID"), sep = "-", remove = FALSE) %>%
     relocate(waferID:dieID, .after = SN)
   
-  cat("saving liv chunk data to file...\n")
+  message("saving liv summary chunk data to file...")
   data.table::fwrite(
     x = df_summary_liv,
-    file = paste0("./data/", config_info$lotID, "_summary_liv.csv"),
+    file = here("data/output", paste0(config_info$lotID, "_summary_liv.csv")),
     append = append_mode
   )
   append_mode = TRUE
@@ -68,60 +69,71 @@ rm(df_liv, this_df_liv, df_summary_liv)
 #### OSA DATA SUMMARIZATION ####
 
 # from csv
-df_osa = data.table::fread(file = paste0("./data/", config_info$lotID, "_combined_OSA.csv")) |>
+df_osa = data.table::fread(here("data/processed", paste0(config_info$lotID, "_combined_OSA.csv"))) |>
   rename(If_osa = If)
 
 df_osa = df_osa |> 
-  group_by(lotID, SN, tempC, If_osa) |> 
+  # group_by(filename, lotID, SN, tempC, If_osa) |> 
+  group_by(filename, lotID, SN, tempC) |> 
   mutate(group_id = cur_group_id()) |> 
   ungroup()
 
-chunk_len = 1000
+# chunk_len = 1000
+chunk_len = 300
 idx1 = 1
 idx2 = idx1 + chunk_len - 1
 max_group_id_osa = max(unique(df_osa$group_id))
 append_mode = FALSE
 while (TRUE) {
-
+  
   if (idx2 > max_group_id_osa) {
     idx2 = max_group_id_osa
   }
-
+  
   # write message updates to console
-  print(paste0("idx1 = ", idx1))
-  print(paste0("idx2 = ", idx2))
+  message(paste0("idx1 = ", idx1))
+  message(paste0("idx2 = ", idx2))
   
   this_df_osa = df_osa |> 
     filter(group_id %in% idx1:idx2)
-
-df_summary_osa = this_df_osa %>%
-  # rename(If_osa = If) |>
-  group_by(lotID, SN, tempC, If_osa) |>
-  summarize(Lp = extract_peak_wav(wavelength, power),
-            Pp = extract_peak_power(power),
-            SMSR = extract_smsr(wavelength, power),
-            BW20dB = extract_bw20dB(wavelength, power),
-            .groups = "drop") %>%
-  ungroup() |>
-  mutate(If_osa = If_osa / 1e-3)
-
-# df_summary_osa = df_summary_osa %>%
-#   tidyfast::dt_separate(SN, into = c("waferID", "cellID", "barID", "dieID"), sep = "-", remove = FALSE) %>%
-#   relocate(waferID:dieID, .after = SN)
-
-df_summary_osa_wide = df_summary_osa %>%
-  # mutate(ID = row_number()) %>%
-  group_by(SN, tempC) %>%
-  mutate(If_index = seq_along(If_osa)) %>%
-  ungroup() %>%
-  pivot_wider(names_from = If_index,
-              values_from = c(If_osa, Lp, Pp, SMSR, BW20dB),
-              names_sep = "")
-
-  cat("saving osa chunk data to file...\n")
+  
+  df_summary_osa = this_df_osa %>%
+    # rename(If_osa = If) |>
+    group_by(filename, lotID, SN, tempC, If_osa) |>
+    summarize(Lp = extract_peak_wav(wavelength, power),
+              Pp = extract_peak_power(power),
+              SMSR = extract_smsr(wavelength, power),
+              BW20dB = extract_bw20dB(wavelength, power),
+              .groups = "drop") %>%
+    ungroup() |>
+    mutate(If_osa = If_osa / 1e-3)
+  
+  # df_summary_osa = df_summary_osa %>%
+  #   tidyfast::dt_separate(SN, into = c("waferID", "cellID", "barID", "dieID"), sep = "-", remove = FALSE) %>%
+  #   relocate(waferID:dieID, .after = SN)
+  
+  df_summary_osa_wide = df_summary_osa %>%
+    # mutate(ID = row_number()) %>%
+    group_by(SN, tempC) %>%
+    mutate(If_index = seq_along(If_osa)) %>%
+    ungroup() %>%
+    pivot_wider(names_from = If_index,
+                values_from = c(If_osa, Lp, Pp, SMSR, BW20dB),
+                names_sep = "")
+  
+  
+  
+  # debug duplicate SN rows
+  dup_rows = df_summary_osa_wide |> 
+    group_by(SN) |> 
+    filter(n() > 1) |> 
+    nrow()
+  
+  
+  message("saving osa summary chunk data to file...\n")
   data.table::fwrite(
     x = df_summary_osa_wide,
-    file = paste0("./data/", config_info$lotID, "_summary_osa.csv"),
+    file = here("data/output", paste0(config_info$lotID, "_summary_osa.csv")),
     append = append_mode
   )
   append_mode = TRUE
@@ -131,14 +143,14 @@ df_summary_osa_wide = df_summary_osa %>%
   if (idx1 > max_group_id_osa) {
     break
   }
-
-}  
   
+}
+
 rm(df_osa, this_df_osa, df_summary_osa_wide)
 
 #### COMBINE SUMMARIES ####
-df_summary_liv = data.table::fread(file = paste0("./data/", config_info$lotID, "_summary_liv.csv"))
-df_summary_osa = data.table::fread(file = paste0("./data/", config_info$lotID, "_summary_osa.csv"))
+df_summary_liv = data.table::fread(here("data/output", paste0(config_info$lotID, "_summary_liv.csv")))
+df_summary_osa = data.table::fread(here("data/output", paste0(config_info$lotID, "_summary_osa.csv")))
 df_summary = full_join(df_summary_liv, df_summary_osa)
 df_summary = df_summary |> mutate(across(where(is.numeric), \(x) round(x, 4)))
-data.table::fwrite(df_summary, file = paste0("./data/", config_info$lotID, "_summary.csv"))
+data.table::fwrite(df_summary, here("data/output", paste0(config_info$lotID, "_summary.csv")))
